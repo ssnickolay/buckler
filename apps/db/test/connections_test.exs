e98cb92acd4b2bdd89679acd2b1bbaa2557539e1
Sverchkov Nikolay <ssnikolay@gmail.com>
2017-10-04 22:26:36 +0300

    :green_heart:

--- apps/db/test/connections_test.exs
+++ apps/db/test/connections_test.exs
@@ -6,17 +6,16 @@ defmodule DB.ConnectionsTest do
     user = create_customer()
     %{user_id: user_id, chat_id: chat_id} = user
 
-    IO.puts(inspect(Repo.all(Customer)))
     assert %Customer{} = Connections.get_chatuser(chat_id, user_id)
   end
 
   test "get_or_create_chat should return persisted chat" do
     chat = create_chat()
-    assert Connections.get_or_create_chat(chat.id) == chat
+    assert Connections.get_or_create_chat(chat.id) == {:ok, chat}
   end
 
   test "get_or_create_chat should create new chat" do
     chat_id = 123
-    assert %Chat{id: 123} = Connections.get_or_create_chat(123)
+    assert {:ok, %Chat{id: 123}} = Connections.get_or_create_chat(123)
   end
 end
--- apps/db/test/support/factory.ex
+++ apps/db/test/support/factory.ex
@@ -2,7 +2,7 @@ defmodule DB.Factory do
   alias DB.{Customer, Chat, Repo, Connections}
 
   def create_customer(_options \\ %{}) do
-    Customer.changeset(%Customer{}, %{
+    %Customer{
       name: "Adam Moris",
       user_id: 4321,
       connected_message_id: 3123,
@@ -10,8 +10,10 @@ defmodule DB.Factory do
       answer: "20",
       lang: "ru",
       attempts: 3,
-      chat: %{id: 123456, lang: "ru", attempts: 3}
-    }) |> Repo.insert!
+    }
+    |> Ecto.Changeset.change
+    |> Ecto.Changeset.put_assoc(:chat, Chat.changeset(%Chat{}, %{id: 123456, lang: "ru", attempts: 3}))
+    |> Repo.insert!
   end
 
   def create_chat(_options \\ %{}) do
