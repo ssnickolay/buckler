54853d3e299f3c3ab2af24d829c53d2f53c90e6a
Sverchkov Nikolay <ssnikolay@gmail.com>
2017-10-06 19:10:09 +0300

    Completed specs for DB.Connections

--- apps/db/test/connections_test.exs
+++ apps/db/test/connections_test.exs
@@ -3,8 +3,8 @@ defmodule DB.ConnectionsTest do
   alias DB.{Customer, Chat, Connections}
 
   test "#get_chatuser should return %Customer{}" do
-    user = create_customer()
-    %{user_id: user_id, chat_id: chat_id} = user
+    customer = create_customer()
+    %{user_id: user_id, chat_id: chat_id} = customer
 
     assert %Customer{} = Connections.get_chatuser(chat_id, user_id)
   end
@@ -28,5 +28,30 @@ defmodule DB.ConnectionsTest do
       chat_id: 789
     }} = Connections.connect_user(789, 123, "John", "answer", 456)
     assert %Chat{} = Repo.get(Chat, 789)
+  end
+
+  test "#decrease_attempts" do
+    customer = create_customer()
+    assert customer.attempts == 3
+    assert {:ok, %Customer{
+      attempts: 2,
+      answer: "another answer"
+    }} = Connections.decrease_attempts(customer.chat_id, customer.user_id, "another answer")
+  end
+
+  test "#delete_chatuser" do
+    customer = create_customer()
+    Connections.delete_chatuser(customer.chat_id, customer.user_id)
+
+    assert is_nil(Connections.get_chatuser(customer.chat_id, customer.user_id))
+  end
+
+  test "#user_unauthorized? when user exists" do
+    customer = create_customer()
+    assert Connections.user_unauthorized?(customer.chat_id, customer.user_id)
+  end
+
+  test "#user_unauthorized? when user non exists" do
+    refute Connections.user_unauthorized?(1, 2)
   end
 end
